8243ff2b67f51eed0a4860c17244f82374ce8bb7
Jc1997 <jc1997@bard.edu>
2016-08-17 10:07:53 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,7 +4,7 @@
  * L&T quote generator
  */
 
-var things = ["Puppies", "Turltes", "Kitties", "My will to live", "Sloths", "Help"];
+var things = ["Puppies", "Turtles", "Kitties", "My-will-to-live", "Sloths", "Help"];
 var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
